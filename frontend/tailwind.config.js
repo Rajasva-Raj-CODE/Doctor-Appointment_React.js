30504b4d305363ffbab0669e25b5e6714fb49a8e
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-05 23:17:04 +0530

    topdoctors done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'primary':"#5f6FFF"
-      }
+      colors: {
+        primary: "#5f6fff", // Hex color codes are case-insensitive, but lowercase is preferred for consistency
+      },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
     },
   },
   plugins: [],
-}
+};
