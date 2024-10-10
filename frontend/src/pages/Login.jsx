fae1e6455f202e866161df512d096fd0832e376f
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-10 23:50:45 +0530

    my-profile done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,38 +15,70 @@ const Login = () => {
   return (
     <form className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
-        <p>{state === "Sign Up" ? "Create Account" : "Login"} </p>
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}{" "}
+        </p>
         <p>
           Please {state === "Sign Up" ? "sign up" : "log in"} to book
           appointment
         </p>
-        <div>
-          <p>Full Name</p>
-          <input
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-        </div>
 
-        <div>
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="border border-zinc-300 w-full p-2 mt-1"
+            />
+          </div>
+        )}
+       
+        <div className="w-full">
           <p>Email</p>
           <input
             type="email"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="border border-zinc-300 w-full p-2 mt-1"
           />
         </div>
 
-        <div>
+        <div className="w-full">
           <p>Password</p>
           <input
             type="password"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="border border-zinc-300 w-full p-2 mt-1"
           />
         </div>
-        <button>{state === "Sign Up" ? "Create Account" : "Login"}</button>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              {" "}
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              click here
+            </span>
+          </p>
+        )}
       </div>
     </form>
   );
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,7 +1,25 @@
-import React from "react";
- 
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const MyAppointments = () => {
-  return <div></div>;
+  const { doctors } = useContext(AppContext);
+  return (
+    <div>
+      <p>My appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
